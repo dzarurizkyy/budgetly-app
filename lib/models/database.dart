bec863dea13161fbed8a5d7470d2cdb9033d1877
Dzaru Rizky Fathan Fortuna <dzarurizkybusiness@gmail.com>
2025-02-04 00:09:06 +0700

    finish phase 4 : install drift library

---
+++ lib/models/database.dart
@@ -0,0 +1,25 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
+import 'package:expenses_tracker_app/models/categories.dart';
+import 'package:expenses_tracker_app/models/transactions.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(tables: [Categories, Transactions])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
